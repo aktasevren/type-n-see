22f18d29aa11efb286cf0aeafe581dff8e940596
aktasevren <evrenaktas@yahoo.com>
2023-04-05 15:24:26 +0300

    edit mapcities.js

--- src/components/MapCities.js
+++ src/components/MapCities.js
@@ -3,57 +3,92 @@ import { MapContainer, GeoJSON } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'
 import { cities } from '../JSON/cities'
 import { Row, Col, Container, Button, Form, InputGroup } from "react-bootstrap";
-
-
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import alertify from "alertifyjs";
 
 
 
 
 export default function MapCities() {
-    const [inputCities,setInputCities ] = useState([])
+    const [inputCities, setInputCities] = useState([])
     const currentCities = []
 
-    const [input,setInput] = useState("")
+    const [input, setInput] = useState("")
 
     const onChange = (e) => {
-        setInput(e.target.value.toLowerCase())
-      };
+        setInput(e.target.value)
+    };
 
     const onSubmit = (e) => {
-        console.log(input)
-        setInputCities(inputCities => [...inputCities,input] );
+        e.preventDefault()
+
+        if (input === "" || input === "null") {
+            alertify.alert("Error", "Please type something!", function () {
+            });
+        }
+
+        else {
+            if (input[0] === "i") {
+                var str2 = "İ" + input.slice(1)
+            } else if (input[0] === "ö") {
+                var str2 = "Ö" + input.slice(1)
+            } else if (input[0] === "ü") {
+                var str2 = "Ü" + input.slice(1)
+            } else if (input[0] === "ı") {
+                var str2 = "I" + input.slice(1)
+            } else {
+                var str2 = input.charAt(0).toUpperCase() + input.slice(1)
+            }
+            if (currentCities.includes(str2)) {
+                setInputCities(inputCities => [...inputCities, str2]);
+            } else {
+                console.log("HATA")
+            }
+
+        }
     }
 
     useEffect(() => {
-        
-        cities["features"].map((city) => currentCities.push((city.properties.name.toLowerCase())))
-        console.log(currentCities)
-    }, [inputCities])
+        cities["features"].map((city) => currentCities.push((city.properties.name)))
 
+    }, [inputCities, currentCities])
 
 
 
     return (
         <div>
+
             <Container>
                 <Row className="d-flex justify-content-center">
                     <Col lg={4}>
                         <InputGroup style={{ height: 48 }} className="my-5" onChange={onChange}>
                             <Form.Control placeholder="guess cities" />
-                            <Button onClick={onSubmit}  className="btn btn-dark">
+                            <Button onClick={onSubmit} className="btn btn-dark">
                                 <b>ADD</b>
                             </Button>
+
                         </InputGroup>
                     </Col>
                 </Row>
+                <Row className="d-flex justify-content-center" >
+                    <Col lg={4}>
+                        <div className="mb-5">
+                            <h2><b>{inputCities.length + "/" + cities["features"].length}</b></h2>
+                        </div>
+                    </Col>
+                </Row>
             </Container>
-            <MapContainer className='map' center={[40, 35]} zoom={6}>
+
+
+            <MapContainer className='map' center={[39, 35]} zoom={6} scrollWheelZoom={false} zoomControl={false} dragging={false} >
 
 
                 <GeoJSON data={cities}
 
+
                     style={(feature) => {
-                        if (inputCities.includes(feature.properties.name.toLowerCase())) {
+                        if (inputCities.includes(feature.properties.name)) {
                             return {
                                 stroke: true,
                                 color: 'green',
@@ -65,7 +100,7 @@ export default function MapCities() {
                                 stroke: true,
                                 color: 'red',
                                 fill: true,
-                                opacity: 0.1
+                                opacity: 0.3
                             }
                         }
                     }}
